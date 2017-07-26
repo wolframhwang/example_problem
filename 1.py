9a890ece64e88c62727431d509163da02700c54d
wolframhwang <hzw94@naver.com>
2017-07-26 20:01:43 +0900

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+#If you want display image on your monitor code(Image(filename=""))
+Image(filename="notMNIST_small/A/Q0NXaWxkV29yZHMtQm9sZEl0YWxpYy50dGY=.png")
+
+#to get all file names
+fn = os.listdir("notMNIST_small/A"/") #if you want J or B just Write instead A
+fn
